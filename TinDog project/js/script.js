565dbe78287d8b9e0032a0da01f26eafa4718a9f
Muftau Akindele <95242189+muftauakindele@users.noreply.github.com>
2022-01-13 16:46:59 -0600

    Add files via upload

---
+++ TinDog project/js/script.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //Scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //Link scroll function
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
